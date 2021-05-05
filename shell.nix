758116145c9a995e1739f7715c8e61b6036620fa
Jacob Chvatal <jake@isnt.online>
2021-05-04 21:00:37 -0700

    pin nixpkgs

--- shell.nix
+++ shell.nix
@@ -1,10 +1,10 @@
-{ pkgs ? import <nixpkgs> {}}:
+{ pkgs ? import (fetchTarball "github.com/nixos/nixpkgs/archive/d90df566caff6ef84f7bfccc2a2c95496f221d62.tar.gz") {}}:
 
 pkgs.mkShell {
   name="visr-dev-environment";
-  buildInputs = [
-    pkgs.clojure
-    pkgs.nodejs
+  buildInputs = with pkgs; [
+    clojure
+    nodejs
   ];
   shellHook = ''
     npm ci
