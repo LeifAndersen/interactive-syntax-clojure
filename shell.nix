9e44f55cfbe2aea3988c21483af2e523c651712d
Jacob Chvatal <jake@isnt.online>
2021-05-07 10:05:32 -0700

    add comment explaining pin

--- shell.nix
+++ shell.nix
@@ -1,4 +1,5 @@
 { pkgs ? import (fetchTarball "github.com/nixos/nixpkgs/archive/d90df566caff6ef84f7bfccc2a2c95496f221d62.tar.gz") {}}:
+# Pinned to the latest (as of this writing) release of NixOS 20.09
 
 pkgs.mkShell {
   name="visr-dev-environment";
