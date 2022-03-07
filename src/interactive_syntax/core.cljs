049d300672439a6f13b035b78898d5675660a097
Leif Andersen <leif@leifandersen.net>
2022-03-07 14:22:52 -0500

    Better autocmplete exclude list.

--- src/interactive_syntax/core.cljs
+++ src/interactive_syntax/core.cljs
@@ -981,6 +981,19 @@
           (if @running? strings/PAUSE strings/RUN)]
          [:> Button {:variant "danger"} strings/STOP]]]]]]))
 
+(def exclude-autocomplete-keys
+  {8 "backspace", 9 "tab", 13 "enter", 16 "shift", 17 "ctrl",
+   18 "alt", 19 "pause", 20 "capslock", 27 "escape", 33 "pageup",
+   34 "pagedown", 35 "end", 36 "home", 37 "left", 38 "up", 39 "right",
+   40 "down", 45 "insert", 46 "delete", 48 "close paren/0" 91 "left window key",
+   92 "right window key", 93 "select", 107 "add", 109 "subtract",
+   110 "decimal point", 111 "divide", 112 "f1", 113 "f2", 114 "f3",
+   115 "f4", 116 "f5", 117 "f6", 118 "f7", 119 "f8", 120 "f9",
+   121 "f10", 122 "f11", 123 "f12", 144 "numlock", 145 "scrolllock",
+   186 "semicolon", 187 "equalsign", 188 "comma", 189 "dash",
+   190 "period", 191 "slash", 192 "graveaccent", 220 "backslash",
+   221 "close bracket" 222 "quote"})
+
 (defn editor-view [{:keys [menu input output options backing file-changed deps
                            current-file fs visr-commit! insert-visr! cursor
                            scroll]
@@ -1078,8 +1091,7 @@
                    ;;(js/console.log (oget e :keyCode))
                    (when (and (= "auto" @(:autocomplete options))
                               (not (-> this .-state .-completionActive))
-                              (not (contains? #{8 9 13 16 18 27 37 38 39 40
-                                                48 221}
+                              (not (contains? (keys exclude-autocomplete-keys)
                                               (oget e :keyCode))))
                      (ocall codemirror/commands :autocomplete this nil
                             #js {:completeSingle false})))
