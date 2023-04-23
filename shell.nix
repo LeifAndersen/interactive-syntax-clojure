ac45162813a15ce60bbfb5f7df6e8f751356092b
Leif Andersen <leif@leifandersen.net>
2023-04-23 19:27:13 -0400

    Does updating nix help?

--- shell.nix
+++ shell.nix
@@ -1,4 +1,5 @@
-{ pkgs ? import (fetchTarball "github.com/NixOS/nixpkgs/archive/7e9b0dff974c89e070da1ad85713ff3c20b0ca97.tar.gz") {}}:
+{ pkgs ? import <nixpkgs> {} }:
+# { pkgs ? import (fetchTarball "github.com/NixOS/nixpkgs/archive/7e9b0dff974c89e070da1ad85713ff3c20b0ca97.tar.gz") {}}:
 # Pinned to the latest (as of this writing) release of NixOS 21.05
 
 pkgs.mkShell {
